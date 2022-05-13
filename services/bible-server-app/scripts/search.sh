055c7e39a6e395b543e94f586d9baae1d2da8da4
ythomas <ythomas@student.42.fr>
2022-05-13 15:18:50 +0200

    [server-app] - ts config and script created to formate and push data to elastic

---
+++ services/bible-server-app/scripts/search.sh
@@ -0,0 +1,11 @@
+curl --user elastic:elastic123 -XGET --header 'Content-Type: application/json' 'http://localhost:9200/bible/_search?format=json&pretty' -d '{
+  "_source": ["text", "book", "chapter", "verse"],
+  "size": 10,
+  "query" : {
+    "match" : { 
+      "text": {
+        "query": "god"
+      }
+    }
+  }
+}'
