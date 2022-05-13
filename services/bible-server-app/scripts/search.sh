4e6bbfe7ed9f36785b451e07bf290ccea2f4145a
ythomas <ythomas@student.42.fr>
2022-05-14 00:35:32 +0200

    [script] - basic search template for elastic test search

--- services/bible-server-app/scripts/search.sh
+++ services/bible-server-app/scripts/search.sh
@@ -1,5 +1,5 @@
 curl --user elastic:elastic123 -XGET --header 'Content-Type: application/json' 'http://localhost:9200/bible/_search?format=json&pretty' -d '{
-  "_source": ["text", "book", "chapter", "verse"],
+  "_source": ["text", "book", "chapter", "verse", "verseID"],
   "size": 10,
   "query" : {
     "match" : { 
