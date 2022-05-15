cfcd22de3a07f770b9544ff3b952df4a4e1f4f39
ythomas <ythomas@student.42.fr>
2022-05-15 13:10:36 +0200

    [client-app] - SearchBar component updated
    => highlight are now 90% efficient, needle is splited by " " to match every single words
    of the user input. the haystack now have a complex regex apply to him to split the text depend on
    every word of the user input
    
    => indentation fixed for the numeric data "book, chapter, verse"
    
    => Route and redirection added onClick on a reshearch result

--- services/bible-client-app/src/components/searchBar.js
+++ services/bible-client-app/src/components/searchBar.js
@@ -1,18 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import "./searchBar.css";
+
+function getFormatedDigit(value){
+  const converted = String(value);
+
+  if (converted.length < 2) return "0" + converted
+  return converted
+}
+
+function isMatching(toMatch, needleTab) {
+  for (let i = 0; i < needleTab.length; i +=1) {
+    if (toMatch.toLowerCase() === needleTab[i].toLowerCase()) return true;
+  }
+  return false;
+}
+
+function buildRegexString(needle) {
+  const tab = needle.split(" ")
+  let result = ""
+  for (let i=0; i<tab.length; i+=1) {
+    result += tab[i];
+    if (i + 1 < tab.length && tab[i].replace(/[ ]*/g, "").length > 1) result += "|"
+  }
+  return result
+}
+
+function getHilightMatch(haystack, needle) {
+  const regex = buildRegexString(needle)
+  return haystack.replace(/[-_.?,;:]+/g, "").split(new RegExp(`(${regex})`, `gi`)).map((piece, index) => {
+    return (
+      <span
+        key={index}
+        style={{
+          background:
+            isMatching(piece, needle.split(" "))
+              ? "#DF9DFC"
+              : "TRANSPARENT",
+          borderRadius: "6px",
+        }}
+      >
+        {piece}
+      </span>
+    )
+  })
+}
 
 export function SearchBar() {
 
-  async function getData(text) {
-    if (text.length < 1) return;
+  const [data, setData] = useState([])
+  const [unserInput, setUserInput] = useState("")
 
-    const data = {
-      data: text,
-    }
+  async function getData(text) {
+    let payload = { data: text }
+    if (text.length < 1) payload = { data: " " }
 
     const queryResult = await fetch("http://localhost:5000/bible/search/searchInBible", {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(payload),
       headers: {
         "Content-Type": "application/json",
         "Accept": "*/*",
@@ -26,17 +71,21 @@ export function SearchBar() {
         console.warn(error)
         return null
       });
-    console.log(queryResult);
+    setData(queryResult)
   }
 
-  const data = [{ "name": "Nopanda" }, { "name": "Yespanda" }, { "name": "DoncTouchMyPanda" }]
-  const ref = "https://www.google.com/"
+  const onchangeHandler = (data) => {
+    getData(data)
+    setUserInput(data);
+  }
+
+  const ref = "http://localhost:3000/"
   return (
     <div className="search" >
       <div className="searchInputs">
         <input
           type="text"
-          onChange={(e) => getData(e.target.value)}
+          onChange={(e) => onchangeHandler(e.target.value)}
           name="search-bar"
           id="search-bar"
           placeholder="Search ..."
@@ -46,11 +95,26 @@ export function SearchBar() {
         </div>
       </div>
       <div className="dataResult">
-        {data.map((value, key) => {
-          return <a className='dataItem' href={ref} target="_blank" rel="noreferrer">
-            <p>{value.name}</p>
-          </a>
+        <div className='dataLegend'>
+          <p className='bookLegend'>Book</p>
+          <p className='chapterLegend'>Chapter</p>
+          <p className='verseLegend'>Verse n°</p>
+          <p className='textLegend'> Verse text</p>
+        </div>
+        <div className='dataItemsContainer'>
+          {data.map((value, key) => {
+          return <Link className='dataItem' to={{
+            pathname: `/lecture/${value.book}/${value.chapter}/${value.verse}`
+          }} state="{value}" target="_blank">
+            <div className='dataItemContent'>
+              <p className='book'>{getFormatedDigit(value.book)}</p>
+              <p className='chapter'>{getFormatedDigit(value.chapter)}</p>
+              <p className='verse'>{getFormatedDigit(value.verse)}</p>
+              <p className='text'>{ getHilightMatch(value.text, unserInput)}</p>
+            </div>
+          </Link>
         })}
+        </div>
       </div>
     </div>
   )
