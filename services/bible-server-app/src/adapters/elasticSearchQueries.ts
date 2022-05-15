dc34c0511905b924acef7e9f20f2bd8221472bff
ythomas <ythomas@student.42.fr>
2022-05-15 13:31:18 +0200

    [server-app] - New elastic adapter for boolean match on more than one field (book && chapter)

--- services/bible-server-app/src/adapters/elasticSearchQueries.ts
+++ services/bible-server-app/src/adapters/elasticSearchQueries.ts
@@ -17,9 +17,11 @@ export async function matchUserQueryInDatabase(
         size: 15,
         body: {
           query: {
-            match_phrase: {
-              text: userQuery,
-            },
+            match: {
+              "text": {
+                "query": userQuery
+              }
+            }
           },
         },
       },
@@ -39,7 +41,70 @@ export async function matchUserQueryInDatabase(
         book: elem._source.book,
         chapter: elem._source.chapter,
         verse: elem._source.verse,
-        Text: elem._source.text,
+        text: elem._source.text,
+      };
+    })
+  );
+  client.close();
+  return result;
+}
+
+export async function getFullChapterInDatabase(
+  book: number,
+  chapter: number,
+  indexName: string
+): Promise<Array<BibleInterface>> {
+  const client = new Client({
+    node: process.env.ELASTIC_URI,
+  });
+
+  const response = await client
+    .search(
+      {
+        index: indexName,
+        _source: ["book", "chapter", "verse", "text"],
+        size: 100,
+        body: {
+          sort: {
+            "verse": {
+              order: "asc"
+            }
+          },
+          query: {
+            bool: {
+              "must": [
+                {
+                  "match": {
+                    "book": book,
+                  }
+                },
+                {
+                  "match": {
+                    "chapter": chapter,
+                  }
+                }
+              ]
+            }
+          },
+        },
+      },
+      {
+        ignore: [404],
+        maxRetries: 3,
+      }
+    )
+    .catch((err) => {
+      // DEAL WITH ERRORS HERE
+      throw err.stack;
+    });
+  if (response.body?.hits?.length < 0) return [];
+  const result = await Promise.all(
+    response.body.hits.hits.map(async (elem: any) => {
+      return {
+        book: elem._source.book,
+        chapter: elem._source.chapter,
+        verse: elem._source.verse,
+        text: elem._source.text,
       };
     })
   );
