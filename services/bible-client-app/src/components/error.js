e9d1dd415704ea79b7c661d395b3e1448f2cae80
ythomas <ythomas@student.42.fr>
2022-05-15 13:08:36 +0200

    [client-app] - error component added for the 404 route

---
+++ services/bible-client-app/src/components/error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./error.css";
+
+export function ErrorPage() {
+  return (
+    <div className='errorPage'>
+      <p className='err404'>404</p>  
+    </div>
+  )
+}
