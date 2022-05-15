fde222d5a92c12c1c3b3351ace6cc28d40ac76e2
ythomas <ythomas@student.42.fr>
2022-05-15 13:29:29 +0200

    [server-app] - formater added to the parser securise number input

--- services/bible-server-app/src/services/bibleResearch.ts
+++ services/bible-server-app/src/services/bibleResearch.ts
@@ -1,8 +1,13 @@
-import { matchUserQueryInDatabase } from "../adapters";
+import { matchUserQueryInDatabase, getFullChapterInDatabase } from "../adapters";
 import { BIBLE_INDEX_NAME } from "../config";
 import { BibleInterface } from "../interfaces"
 
 export async function searchBibleVerse(userQuery: string): Promise<Array<BibleInterface>> {
   const result = await matchUserQueryInDatabase(userQuery, BIBLE_INDEX_NAME);
+  return result;
+}
+
+export async function searchBibleFullChapter(book: number, chapter: number): Promise<Array<BibleInterface>> {
+  const result = await getFullChapterInDatabase(book, chapter, BIBLE_INDEX_NAME);
   return result;
 }
--- services/bible-server-app/src/services/parser.ts
+++ services/bible-server-app/src/services/parser.ts
@@ -1,10 +1,47 @@
-import {USER_INPUT_MAX_LENGTH, USER_INPUT_MIN_LENGTH} from "../config";
+import { isNumberObject } from "util/types";
+import {
+  USER_INPUT_MAX_LENGTH,
+  USER_INPUT_MIN_LENGTH,
+  BOOK_MAX,
+  BOOK_MIN,
+  CHAPTER_MAX,
+  CHAPTER_MIN,
+} from "../config";
 
 export async function userInputParser(str: string): Promise<string | null> {
   const inputFormated = str
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[ ]{2,}/g, " ")
-  if (str.length < USER_INPUT_MIN_LENGTH || str.length > USER_INPUT_MAX_LENGTH) return null;
+    .replace(/[ ]{2,}/g, " ");
+  if (str.length < USER_INPUT_MIN_LENGTH || str.length > USER_INPUT_MAX_LENGTH)
+    return null;
   return inputFormated;
+}
+
+async function formatNumber(str: string) {
+  return str.replace(/[^0-9]+/g, "");
+}
+
+export async function bookParser(book: number): Promise<number | null> {
+  const formatedBook = Number(await formatNumber(String(book)))
+  if (
+    formatedBook === null ||
+    formatedBook === undefined ||
+    formatedBook < BOOK_MIN ||
+    formatedBook > BOOK_MAX
+  )
+    return null;
+  return formatedBook;
+}
+
+export async function chapterParser(chapter: number): Promise<number | null> {
+  const formatedChapter = Number(await formatNumber(String(chapter)))
+  if (
+    formatedChapter === null ||
+    formatedChapter === undefined ||
+    formatedChapter < CHAPTER_MIN ||
+    formatedChapter > CHAPTER_MAX
+  )
+    return null;
+  return formatedChapter;
 }
