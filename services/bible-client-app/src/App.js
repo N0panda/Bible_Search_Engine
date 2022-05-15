b6883822fe3bdc1353ffcd86d6920316753dbd66
ythomas <ythomas@student.42.fr>
2022-05-15 13:29:58 +0200

    [client-app] - Main router added to the app

--- services/bible-client-app/src/App.js
+++ services/bible-client-app/src/App.js
@@ -1,11 +1,23 @@
-import { MainTitle,  SearchBar } from "./components";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css"
+import { ErrorPage } from "./components/error";
+import { ReadChapter } from "./components/lecturePage"
+import { SearchPage } from "./components/searchPage";
+
+
 
 export default function App() {
   return (
-    <div className="App">
-      <MainTitle />
-      <SearchBar />
-    </div>
+    <BrowserRouter>
+      <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="/search" />}/>
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/lecture/:book/:chapter/:verse" element={<ReadChapter />} />
+            <Route path="*" element={<Navigate to="/search" />} />
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
