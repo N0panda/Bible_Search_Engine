28a5b0e2cd9ea06c568252ce08d34295e898e647
ythomas <ythomas@student.42.fr>
2022-05-16 15:35:03 +0200

    creation script get from develop branch / setup dosn't sleep anymore

--- services/bible-server-app/scripts/createElasticIndexWithFullBible.ts
+++ services/bible-server-app/scripts/createElasticIndexWithFullBible.ts
@@ -1,81 +1,154 @@
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 import { Client } from "@elastic/elasticsearch";
 import { ELASTIC_URI, INDEX_NAME } from "./.config";
 
 async function createElasticIndex(client: Client): Promise<void> {
   const bibleDataTemplate = {
-    "settings": {
+    settings: {
       number_of_shards: 1,
       number_of_replicas: 0,
       analysis: {
+        // Analyzer used
         analyzer: {
-          my_analyzer: {
+          index_ngram_analyzer: {
             tokenizer: "standard",
-            // filter: [
-            //   "lowercase", "ngram"
-            // ]
-          }
+            filter: ["lowercase", "index_ngram_filter", "stop"],
+          },
+          search_ngram_analyzer: {
+            tokenizer: "standard",
+            filter: ["stop"],
+          },
         },
-        // filter: {
-        //   ngram: {
-        //     type: "edge_ngram",
-        //     min_gram: 1,
-        //     max_gram: 20,
-        //     token_chars: [
-        //       "letter",
-        //       "digit"
-        //     ]
-        //   }
-        // }
-      }
+        // ######## Custom filter creation #########
+        filter: {
+          index_ngram_filter: {
+            type: "edge_ngram",
+            min_gram: 2,
+            max_gram: 20,
+            token_chars: ["letter", "digit"],
+          },
+          search_ngram_filter: {
+            type: "edge_ngram",
+            min_gram: 2,
+            max_gram: 20,
+            token_chars: ["letter", "digit"],
+          },
+        },
+        // ######## Custom tokenizer creation (ATM not used)#########
+        tokenizer: {
+          edge_ngram_tokenizer: {
+            type: "edge_ngram",
+            min_gram: 2,
+            max_gram: 4,
+          },
+        },
+      },
     },
     mappings: {
       properties: {
-        verseID: { type: "keyword" },
-        book: { type: "short" },
-        chapter: { type: "short" },
-        verse: { type: "short" },
-        text: { type: "text" },
+        verseID: {
+          type: "keyword",
+        },
+        book: {
+          type: "short",
+        },
+        chapter: {
+          type: "short",
+        },
+        verse: {
+          type: "short",
+        },
+        text: {
+          type: "text",
+          analyzer: "index_ngram_analyzer",
+          search_analyzer: "standard",
+        },
       },
     },
   };
   try {
-    await client.indices
-    .create({
+    await client.indices.create({
       index: INDEX_NAME,
       wait_for_active_shards: "1",
       body: bibleDataTemplate,
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-  console.log(`${INDEX_NAME} has been created`)
+  console.log(`${INDEX_NAME} has been created`);
 }
 
 async function deleteElasticIndex(client: Client): Promise<void> {
-    await client.indices.delete({
-      index: INDEX_NAME
-    }).then(() => {
-      console.log(`${INDEX_NAME} has been deleted`)
-    }).catch((err) => {
-      console.error("Index does not exist")
+  await client.indices
+    .delete({
+      index: INDEX_NAME,
     })
+    .then(() => {
+      console.log(`${INDEX_NAME} has been deleted`);
+    })
+    .catch((err) => {
+      console.error("Index does not exist");
+    });
 }
 
 async function fillElasticIndex(client: Client): Promise<void> {
+  const data = JSON.parse(
+    await readFile("./ressources/KJVBibleParsed.json", "utf8")
+  );
 
-  const data = JSON.parse(await readFile("./ressources/KJVBibleParsed.json", "utf8"));
-  
+  const body = await data.flatMap((doc: any) => [
+    { index: { _index: "bible" } },
+    doc,
+  ]);
+  await client
+    .bulk({
+      refresh: true,
+      body: body,
+    })
+    .catch((err) => {
+      console.log(err);
+      return;
+    });
+  console.log("Database correctly Filles !");
+}
 
-  const body = await data.flatMap((doc: any) => [{ index: { _index: 'bible' } }, doc])
-  await client.bulk({
-    refresh: true,
-    body: body,
-  }).catch((err) => {
-    console.log(err)
-    return;
-  })
-  console.log("Database correctly Filles !")
+async function waitForUp(client: Client) {
+  let connected = false;
+  let attempt = 0;
+  let error = null;
+  const maxAttempt = 10;
+  while (attempt < maxAttempt) {
+    connected = await client
+      .ping(
+        {},
+        {
+          requestTimeout: 2000,
+        }
+      )
+      .then((res) => {
+        return true;
+      })
+      .catch((err) => {
+        attempt += 1;
+        error = err;
+        console.warn(
+          `Trying to reconect => Attempts : [${attempt}/${maxAttempt}]`
+        );
+        return false;
+      });
+    if (connected) {
+      console.log("Client connected !");
+      return {
+        connected: true,
+        error: null,
+      };
+    }
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+  return {
+    connected: false,
+    error,
+  };
 }
 
 async function setupElasticDb(): Promise<void> {
@@ -83,11 +156,16 @@ async function setupElasticDb(): Promise<void> {
     node: ELASTIC_URI,
   });
 
+  const status = await waitForUp(client);
+  if (!status.connected) {
+    console.warn(status.error);
+    return;
+  }
   await deleteElasticIndex(client);
-  console.log(`Creation of ${INDEX_NAME} ...`)
+  console.log(`Creation of ${INDEX_NAME} ...`);
   await createElasticIndex(client);
-  console.log("Filling Database ...")
-  await fillElasticIndex(client)
+  console.log("Filling Database ...");
+  await fillElasticIndex(client);
 }
 
 setupElasticDb();
--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ docker-compose up -d
 
 ## Setup the database
 echo "Waiting for the database to setup ..."
-sleep 7
 
 ## setup the database
 docker exec bible-server-app npm run elastic:init_data
